2fb815bd686382b66431e4483faa92c50dd30758
Cyndja Aryelle <168868280+aryzinhasz@users.noreply.github.com>
2025-09-24 21:30:04 -0300

    Create classeRetangulo.js

---
+++ Classes/classeRetangulo.js
@@ -0,0 +1,44 @@
+class Retangulo {
+  constructor(altura, largura) {
+    this.altura = altura;
+    this.largura = largura;
+  }
+
+  //Getter
+  get area() {
+    return this.calculaArea();
+  }
+
+  calculaArea() {
+    return this.altura * this.largura;
+  }
+}
+
+const quadrado = new Retangulo(10, 10);
+
+console.log(quadrado.area);
+
+class Ponto {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+  }
+
+  static distancia(a, b) {
+    const dx = a.x - b.x;
+    const dy = a.y - b.y;
+
+    return Math.hypot(dx, dy);
+  }
+}
+
+const p1 = new Ponto(5, 5);
+const p2 = new Ponto(10, 10);
+
+p1.distancia; //undefined
+p2.distancia; //undefined
+
+console.log(Ponto.distancia(p1, p2));
+
+Retangulo.larguraEstatico = 20;
+Retangulo.prototype.larguraPrototipagem = 25;
